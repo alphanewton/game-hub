3796f8f92c8223871808d258888b9062d104e245
Newton Narzary <newto@Newtons-MacBook-Air.local>
2023-10-25 13:26:02 +0530

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,15 +15,19 @@ export interface Game {
   metacritic: number;
 }
 
-function useGames(selectedGenre: Genre | null) {
+function useGames(
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) {
   return useData<Game>(
     "/games",
     {
       params: {
         genres: selectedGenre?.id,
+        platforms: selectedPlatform?.id,
       },
     },
-    [selectedGenre?.id]
+    [selectedGenre?.id, selectedPlatform?.id]
   );
 }
 
